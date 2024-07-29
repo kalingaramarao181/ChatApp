4fbf75e39cc349ecd399d9a848a77c7d0236be64
attadasuresh <sureshattada12233@gmail.com>
2024-07-30 00:16:50 +0530

    second

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import Signin from './Components/Signin';
+import Login from './Components/Login';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
 <Navbar/>
+    <Switch>
+      <Route path='/' exact component={Home} />
+      <Route path='/Login' exact component={Login} />
+      <Route path='/Signin' exact component={Signin} />
+    </Switch>
+    </BrowserRouter>
 
     </>
   );
--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
+import './Navbar.css';
 const Navbar = () => {
   return (
 <>
+<div>
 
-<li>Home</li>
-<li>Login</li>
-<li>Signin</li>
+<ul className='Navbar-container'>
+<Link to='/'><li className='navbar-elements'>Home</li></Link>
+<Link to='/Login'><li  className='navbar-elements'>Login</li></Link>
+<Link to='/Signin'><li  className='navbar-elements'>Signin</li></Link> 
+</ul>
 
-
+</div>
 </> 
  )
 }
